1debc2d273b85a758e5e6584bfeb1e206b3ea13e
robozombiex <robozombiex@gmail.com>
2026-07-01 20:04:51 -0300

    Add files via upload

---
+++ Protótipo/dicionario-de-libras/dicionario-de-libras/sinal.gd
@@ -0,0 +1,17 @@
+extends Node
+class_name sinal
+
+enum Assunto {
+	NENHUM = 0
+}
+
+var palavra
+var ID
+var origem
+var classe
+var exemplo
+var exemplo_libras
+var imagem
+var mao
+var video
+var acepcao : String
